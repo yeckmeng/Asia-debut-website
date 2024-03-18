0de2614708fef20b89231c38838a6be2cfb7bd25
faizdaudAsiaDebut <faiz@asiadebut.group>
2024-03-18 09:58:01 +0800

    add firebase config

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCXVcKPrhFgTmhsjXhtA0aFsq_zY3-4h9M",
+    authDomain: "asia-debut.firebaseapp.com",
+    projectId: "asia-debut",
+    storageBucket: "asia-debut.appspot.com",
+    messagingSenderId: "518880234878",
+    appId: "1:518880234878:web:4dc6fc8a835ea7b35df72f"
+  };
