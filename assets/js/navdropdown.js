692635ea5b1b6ac05abc448db815e6b82af43331
Yeck Meng Phoon <yeckmengphoon@gmail.com>
2024-03-27 12:17:36 +0800

    changes #17

--- assets/js/navdropdown.js
+++ assets/js/navdropdown.js
@@ -1,15 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Get the Services dropdown link
-    var servicesDropdown = document.getElementById('servicesDropdownList');
+    // Function to close all dropdown menus
+    function closeAllDropdowns() {
+        var dropdownMenus = document.querySelectorAll('.dropdown-menu');
+        dropdownMenus.forEach(function (menu) {
+            menu.classList.remove('show');
+        });
+    }
 
-    // Add click event listener to the Services dropdown link
-    servicesDropdown.addEventListener('click', function () {
-        // Toggle the dropdown-menu
-        var servicesDropdownMenu = document.getElementById('servicesDropdown');
-        if (servicesDropdownMenu.classList.contains('show')) {
-            servicesDropdownMenu.classList.remove('show');
-        } else {
-            servicesDropdownMenu.classList.add('show');
-        }
-    });
+    // Function to toggle dropdown menu
+    function toggleDropdown(dropdownToggleId, dropdownMenuId) {
+        var dropdownToggle = document.getElementById(dropdownToggleId);
+        dropdownToggle.addEventListener('click', function () {
+            var dropdownMenu = document.getElementById(dropdownMenuId);
+            if (dropdownMenu.classList.contains('show')) {
+                dropdownMenu.classList.remove('show');
+            } else {
+                closeAllDropdowns(); // Close all dropdowns before opening the clicked one
+                dropdownMenu.classList.add('show');
+            }
+        });
+    }
+
+    // Call the function for Services dropdown
+    toggleDropdown('servicesDropdownList', 'servicesDropdown');
+
+    // Call the function for FAQ dropdown
+    toggleDropdown('faqDropdownList', 'faqDropdown');
 });
